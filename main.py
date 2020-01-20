b8cfa696e5881b44d69d9012b65cc91e96d4589c
Enchan <enchant0130@gmail.com>
2020-01-20 20:56:30 +0900

    [Update/Add] description

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 from lib.DBQueue import DBQueue
 import threading
 
-#--デキュースレッド
+#--デキュースレッドを立てる
 def dequeueThread():
     queue4Dequeue = DBQueue()
     queue4Dequeue.connect("db/main.db")
@@ -14,6 +14,7 @@ dqthread = threading.Thread(target=dequeueThread)
 dqthread.setDaemon(True) #デーモンスレッド化しないとタイムアウトするまで終わらなくなる
 dqthread.start()
 
+#--こっちはサンプルスレッド
 def fc1():
     event = threading.Event()
     queue = DBQueue()
@@ -23,6 +24,18 @@ def fc1():
     event.clear()
     print(queue.fetchrst("Client-1"))
 
-thr1 = threading.Thread(target=fc1)
-thr1.start()
+def fc2():
+    event = threading.Event()
+    queue = DBQueue()
+    queue.initClient("Client-2")
+    queue.enQueue("Client-2", event, "INSERT INTO testTable VALUES(0, ?)", ("Bob", ))
+    event.wait()
+    event.clear()
+    print(queue.fetchrst("Client-2"))
 
+thr1 = threading.Thread(target=fc1)
+thr2 = threading.Thread(target=fc2)
+
+thr1.start()
+thr2.start()
+thr1.start()
